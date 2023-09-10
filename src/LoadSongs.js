74ab0902d4b8a1f298f65841554a170d07760d06
SamuelKasper <Samuel.Noah.Kasper@hs-furtwangen.de>
2023-09-10 22:10:57 +0200

    add: custom audioplayer

--- src/LoadSongs.js
+++ src/LoadSongs.js
@@ -19,6 +19,7 @@ function loadSongs() {
         const externalProjects = document.getElementById("external_project_songs");
         yield loadProjects(song_data.own_projects, ownProjects);
         yield loadProjects(song_data.external_projects, externalProjects);
+        playMusic();
     });
 }
 ;
@@ -29,10 +30,17 @@ function loadIndependentSongs(song_data) {
         let independent_songs = song_data.independent_songs;
         independent_songs.forEach((song) => {
             let section = document.createElement("section");
-            section.classList.add("song-section");
+            section.classList.add("audioplayer");
+            // Bildcover
             section.appendChild(createImg(song.img));
+            // Songname
             section.appendChild(createSongName(song.name));
+            // Audio Element
             section.appendChild(createAudio(song.src, song.volume));
+            //Play Button
+            section.appendChild(createButton());
+            // Progressbar
+            section.appendChild(createProgressbar());
             content === null || content === void 0 ? void 0 : content.appendChild(section);
         });
     });
@@ -40,6 +48,7 @@ function loadIndependentSongs(song_data) {
 // Load songs from own and external projects
 function loadProjects(projects, content) {
     return __awaiter(this, void 0, void 0, function* () {
+        let index = 0;
         projects.forEach((project) => {
             let headline = document.createElement("h4");
             let link = document.createElement("a");
@@ -49,10 +58,17 @@ function loadProjects(projects, content) {
             content === null || content === void 0 ? void 0 : content.appendChild(headline);
             project.songs.forEach((song) => __awaiter(this, void 0, void 0, function* () {
                 let section = document.createElement("section");
-                section.classList.add("song-section");
+                section.classList.add("audioplayer");
+                // Bildcover
                 section.appendChild(createImg(song.img));
+                // Songname
                 section.appendChild(createSongName(song.name));
+                // Audio Element
                 section.appendChild(createAudio(song.src, song.volume));
+                //Play Button
+                section.appendChild(createButton());
+                // Progressbar
+                section.appendChild(createProgressbar());
                 content === null || content === void 0 ? void 0 : content.appendChild(section);
             }));
         });
@@ -63,13 +79,15 @@ function createAudio(_src, _vol) {
     let audio = new Audio(_src);
     audio.controls = true;
     audio.volume = _vol;
-    audio.classList.add("audio-control");
+    audio.classList.add("song");
     audio.setAttribute('controlsList', 'nodownload');
+    audio.setAttribute('loop', 'true');
     return audio;
 }
 // Creating the p element for the song name
 function createSongName(_name) {
     let text = document.createElement("p");
+    text.classList.add("songname");
     text.innerText = `${_name}`;
     return text;
 }
@@ -78,5 +96,71 @@ function createImg(_src) {
     let image = document.createElement("img");
     image.src = _src;
     return image;
+}
+// Create progressbar
+function createProgressbar() {
+    let input = document.createElement("input");
+    input.classList.add("progress");
+    input.setAttribute("type", "range");
+    input.setAttribute("value", "0");
+    return input;
+}
+// Create playbutton
+function createButton() {
+    let img = document.createElement("img");
+    img.classList.add("btn");
+    img.src = "images/icons/play.svg";
+    return img;
+}
+// Functionallity of buttons and audio elements
+function playMusic() {
+    var _a;
+    // Click on Button
+    let btns = document.getElementsByClassName("btn");
+    for (let btn of btns) {
+        btn.addEventListener("click", function (e) {
+            let btntemp = btn;
+            let audio = btn.previousElementSibling;
+            let progress = audio.nextElementSibling.nextElementSibling;
+            if (audio.paused) {
+                stopMusic(progress);
+                audio.play();
+                // Move progressbar 
+                progress.max = audio.duration.toString();
+                setInterval(() => {
+                    progress.value = audio.currentTime.toString();
+                }, 500);
+                // Set pause img
+                btntemp.src = "../images/icons/stop.svg";
+            }
+            else {
+                btntemp.src = "../images/icons/play.svg";
+                stopMusic(progress);
+            }
+        });
+    }
+    // Click on Progressbar
+    let progressbars = document.getElementsByClassName("progress");
+    for (let element of progressbars) {
+        let progressbar = element;
+        let audio = (_a = progressbar.previousElementSibling) === null || _a === void 0 ? void 0 : _a.previousElementSibling;
+        progressbar.onchange = function () {
+            audio.currentTime = parseInt(progressbar.value);
+        };
+    }
+}
+// Stops all music and reset play buttons
+function stopMusic(_progress) {
+    let audioElements = document.getElementsByTagName('audio');
+    [...audioElements].forEach((audio) => {
+        audio.pause();
+        /* audio.currentTime = 0; */
+    });
+    let imgElements = document.getElementsByTagName('img');
+    [...imgElements].forEach((img) => {
+        if (img.classList.contains("btn")) {
+            img.src = "../images/icons/play.svg";
+        }
+    });
 }
 window.onload = loadSongs;
--- src/LoadSongs.ts
+++ src/LoadSongs.ts
@@ -10,6 +10,8 @@ async function loadSongs() {
     const externalProjects = document.getElementById("external_project_songs");
     await loadProjects(song_data.own_projects, ownProjects);
     await loadProjects(song_data.external_projects, externalProjects);
+
+    playMusic();
 };
 
 // Load independent songs
@@ -19,16 +21,25 @@ async function loadIndependentSongs(song_data: any) {
 
     independent_songs.forEach((song: { name: string; year: string; img: string; src: string; volume: number; }) => {
         let section = document.createElement("section");
-        section.classList.add("song-section");
+        section.classList.add("audioplayer");
+        // Bildcover
         section.appendChild(createImg(song.img));
+        // Songname
         section.appendChild(createSongName(song.name));
+        // Audio Element
         section.appendChild(createAudio(song.src, song.volume));
+        //Play Button
+        section.appendChild(createButton());
+        // Progressbar
+        section.appendChild(createProgressbar());
         content?.appendChild(section);
     });
 }
 
 // Load songs from own and external projects
 async function loadProjects(projects: any, content: HTMLElement | null) {
+    let index: number = 0;
+
     projects.forEach((project: any) => {
         let headline = document.createElement("h4");
         let link = document.createElement("a");
@@ -39,10 +50,17 @@ async function loadProjects(projects: any, content: HTMLElement | null) {
 
         project.songs.forEach(async (song: { name: string; year: string; src: string; img: string; volume: number; }) => {
             let section = document.createElement("section");
-            section.classList.add("song-section");
+            section.classList.add("audioplayer");
+            // Bildcover
             section.appendChild(createImg(song.img));
+            // Songname
             section.appendChild(createSongName(song.name));
+            // Audio Element
             section.appendChild(createAudio(song.src, song.volume));
+            //Play Button
+            section.appendChild(createButton());
+            // Progressbar
+            section.appendChild(createProgressbar());
             content?.appendChild(section);
         });
     });
@@ -53,14 +71,16 @@ function createAudio(_src: string, _vol: number): HTMLAudioElement {
     let audio = new Audio(_src);
     audio.controls = true;
     audio.volume = _vol;
-    audio.classList.add("audio-control");
+    audio.classList.add("song");
     audio.setAttribute('controlsList', 'nodownload');
+    audio.setAttribute('loop', 'true');
     return audio;
 }
 
 // Creating the p element for the song name
 function createSongName(_name: string): HTMLParagraphElement {
     let text = document.createElement("p");
+    text.classList.add("songname");
     text.innerText = `${_name}`;
     return text;
 }
@@ -70,6 +90,77 @@ function createImg(_src: string): HTMLImageElement {
     let image = document.createElement("img");
     image.src = _src;
     return image;
+}
+
+// Create progressbar
+function createProgressbar() {
+    let input = document.createElement("input");
+    input.classList.add("progress");
+    input.setAttribute("type", "range");
+    input.setAttribute("value", "0");
+    return input;
+}
+
+// Create playbutton
+function createButton() {
+    let img = document.createElement("img");
+    img.classList.add("btn");
+    img.src = "images/icons/play.svg";
+    return img;
+}
+
+// Functionallity of buttons and audio elements
+function playMusic() {
+    // Click on Button
+    let btns = document.getElementsByClassName("btn");
+    for (let btn of btns) {
+        btn.addEventListener("click", function (e) {
+            let btntemp: HTMLImageElement = <HTMLImageElement>btn;
+            let audio: HTMLAudioElement = <HTMLAudioElement>btn.previousElementSibling;
+            let progress: HTMLInputElement = <HTMLInputElement>audio!.nextElementSibling!.nextElementSibling;
+
+            if (audio.paused) {
+                stopMusic(progress);
+                audio.play();
+                // Move progressbar 
+                progress.max = audio.duration.toString();
+                setInterval(() => {
+                    progress.value = audio.currentTime.toString();
+                }, 500);
+                // Set pause img
+                btntemp.src = "../images/icons/stop.svg";
+            } else {
+                btntemp.src = "../images/icons/play.svg";
+                stopMusic(progress);
+            }
+        });
+    }
+
+    // Click on Progressbar
+    let progressbars = document.getElementsByClassName("progress");
+    for (let element of progressbars) {
+        let progressbar: HTMLInputElement = <HTMLInputElement>element;
+        let audio: HTMLAudioElement = <HTMLAudioElement>progressbar.previousElementSibling?.previousElementSibling;
+        progressbar.onchange = function () {
+            audio.currentTime = parseInt(progressbar.value);
+        }
+    }
+}
+
+// Stops all music and reset play buttons
+function stopMusic(_progress: HTMLInputElement) {
+    let audioElements = document.getElementsByTagName('audio');
+    [...audioElements].forEach((audio) => {
+        audio.pause();
+        /* audio.currentTime = 0; */
+    });
+
+    let imgElements = document.getElementsByTagName('img');
+    [...imgElements].forEach((img) => {
+        if (img.classList.contains("btn")) {
+            img.src = "../images/icons/play.svg";
+        }
+    });
 }
 
 window.onload = loadSongs;
